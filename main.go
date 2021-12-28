0850f95c335d8f675e13cfc2bce583f482588035
zealotry17 <96761832+zealotry17@users.noreply.github.com>
2021-12-28 18:48:44 +0900

    change main

--- main.go
+++ main.go
@@ -3,21 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
-	fmt.Println("New Hello world")
+	fmt.Println("New world")
+	fmt.Println("New world")
+	fmt.Println("New world")
 
 }
