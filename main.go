4b44e014b611b7e99658cdb67f816bad81b7639a
zealotry17 <96761832+zealotry17@users.noreply.github.com>
2021-12-29 11:00:33 +0900

    add test code

--- main.go
+++ main.go
@@ -11,4 +11,6 @@ func main() {
 	fmt.Println("New world")
 
 	fmt.Println(calculator.Multyply(2, 2))
+	fmt.Println(calculator.LenAndUpper("Sangwoo Lee"))
+	calculator.RepeatMe("aaa", "bbbb", "cccc", "dddd")
 }
