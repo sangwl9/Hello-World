4b729df1fda563f053cb3e81d796b8611a06c030
zealotry17 <96761832+zealotry17@users.noreply.github.com>
2021-12-30 15:20:01 +0900

    add wrong test case

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -11,4 +11,10 @@ func TestCalculator(t *testing.T) {
 	if ans != 4 {
 		t.Errorf("Multyply(2, 2) = %d; want 4\n", ans)
 	}
+
+	ans = calculator.Multyply(10, 10)
+
+	if ans != 4 {
+		t.Errorf("Multyply(10, 10) = %d; want 4\n", ans)
+	}
 }
