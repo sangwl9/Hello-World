5ba2297ec4d350eb2d6d1b2158fc4656b1836ee3
zealotry17 <96761832+zealotry17@users.noreply.github.com>
2021-12-31 17:42:57 +0900

    change main

--- main.go
+++ main.go
@@ -37,11 +37,4 @@ func main() {
 	fmt.Println(calculator.Multyply(2, 2))
 	fmt.Println(calculator.Multyply(1857, 6589))
 	fmt.Println(calculator.Multyply(2, 2))
-
-	fmt.Println(calculator.Multyply(1857, 6589))
-	fmt.Println(calculator.Multyply(2, 2))
-	fmt.Println(calculator.Multyply(1857, 6589))
-	fmt.Println(calculator.Multyply(2, 2))
-	fmt.Println(calculator.Multyply(1857, 6589))
-	fmt.Println(calculator.Multyply(2, 2))
 }
